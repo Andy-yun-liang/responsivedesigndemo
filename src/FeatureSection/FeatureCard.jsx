9e32e8350231bdcefde588a6f96a5fb3ecd9bd5e
Andy Liang <yf98.liang@gmail.com>
2025-04-04 20:14:27 -0700

    Replaced Hero Component because I thought it looked terrible in mobile, Added featurecard which contains the services we offer, currently only contains stock image, title, and generic description. Made both the Hero Section, and Services Section with the mobile first approach. Have breakpoints at md and 3xl which is 144rem

--- src/FeatureSection/FeatureCard.jsx
+++ src/FeatureSection/FeatureCard.jsx
@@ -1,26 +1,13 @@
+const FeatureCard = (props) => {
+  const { feature } = props;
 
-
-const FeatureCard = (props) =>{
-    const {feature} = props;
-
-    return<div className = "flex items-center justify-start">
-            <div>
-
-            
-            <img className = "w-48" src = {feature.image}/>
-            {feature.title}
-            </div>
-            
-            <div className = "flex justify-start w-72">
-
-            {feature.description}
-            </div>
-            
-
+  return (
+    <div className="flex flex-col items-center m-6 p-6 text-andygold border-andygold bg-white border-2 rounded-xl shadow-xl md:w-[370px] md:h-[480px] 3xl:w-[450px] 3xl:h-[550px]">
+        <img className = "w-[200px]" src = {feature.image}/>
+        <div className = "my-4 text-2xl text-ubuntu">{feature.title}</div>
+        <div className = "text-sm text-start 3xl:text-lg overflow-y-auto">{feature.description}</div>
     </div>
-
-
-
+  );
 };
 
 export default FeatureCard;
