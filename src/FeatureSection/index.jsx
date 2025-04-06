6149cd67ac93d0f3356e45069134280c1ca9e787
Andy Liang <yf98.liang@gmail.com>
2025-04-06 16:25:17 -0700

    Created generic navbar component and services offers section component that are responsive for both mobile and web screen resolutions. Added a framework section that is suppose to represent the roadmap for the services we provide but isnt complete yet. The mobile and tablet version is complete but I need to make a larger SVG to suit widescreen

--- src/FeatureSection/index.jsx
+++ src/FeatureSection/index.jsx
@@ -10,7 +10,7 @@ const FeatureSection = (props) => {
 
   return (
     <div className="flex flex-col items-center justify-center bg-andyblue">
-      <div className="text-3xl font-ubuntu text-andygold text-center mt-10 md:text-5xl 3xl:text-6xl">
+      <div className="text-3xl font-ubuntu text-white text-center mt-10 md:text-5xl 3xl:text-6xl">
         Services We Offer
       </div>
       <div className="w-full max-w-6xl flex flex-col m-10 justify-center items-center md:flex-row md:max-w-7xl md:flex-wrap 3xl:flex-nowrap 3xl:max-w-full" >
---
+++ src/FrameworkChart.jsx
@@ -0,0 +1,17 @@
+import Framework from "./assets/framework.svg"
+
+
+const FrameworkChart = () => {
+
+    return <div className = "flex justify-content items-center">
+        <div className = "w-full max-w-6xl flex justify-center items-center">
+
+            <img src = {Framework}/>
+        </div>
+
+
+    </div>
+
+};
+
+export default FrameworkChart;
