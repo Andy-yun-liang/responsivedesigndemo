85a5bb2fa47e0b4702f56c2bfaed902b3e9f4d27
Andy Liang <andy.yun.liang@gmail.com>
2025-04-17 15:14:38 -0700

    Added JsConfig/ Refactored routing: Added jsconfig to avoid long nested file paths, added login portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
+import Admin from "./pages/Admin";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="font-roboto">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/services" element={<Services/>} />
-          <Route path="/contact" element={<Contact/>} />
-          
-        </Routes>
-        <Footer />
-      </div>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+        <Route path="/admin-portal" element={<Admin />} />
+      </Routes>
     </BrowserRouter>
   );
 };
